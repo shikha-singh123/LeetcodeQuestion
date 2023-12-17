922e6bc7b774e12fa36e9740930be1b9e3d2ea3d
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2023-12-17 10:22:44 +0530

    Time: 1 ms (99.94%) | Memory: 45 MB (21.34%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,13 +1,31 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int n=nums.length;
-        Set<Integer>set=new HashSet<>();
-       for (int num:nums){
-           if(!set.add(num)){
-               set.remove(num);
-           }
+        //1st approach
+    //     Set<Integer>set=new HashSet<>();
+    //    for (int num:nums){
+    //        if(!set.add(num)){
+    //            set.remove(num);
+    //        }
 
-       }
-       return set.iterator().next();
-    }
+    //    }
+    //    return set.iterator().next();
+    //2nd approach
+    // Arrays.sort(nums);
+    // for(int i=0;i<n-1;i+=2){
+    //     if(nums[i]!=nums[i+1]){
+    //         return nums[i];
+    //     }
+    //     }
+    //         return nums[n-1];
+    //     }
+    // }
+    
+//3rd approach
+int xor=0;
+for(int num:nums){
+    xor^=num;
+}
+return xor;
+}
 }
