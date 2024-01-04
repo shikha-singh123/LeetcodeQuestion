1de4486d29be45a5a488d608ad9806150a849451
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2024-01-04 15:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (17.73%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,61 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+     int lb=lowerbound(nums,target);
+     if(lb==nums.length || nums[lb]!=target)
+     return new int []{-1,-1};
 
-        // int n=nums.length;
-        // int left=0;
-        // int right=n-1;
-        // while(left<=right){
-        //    int mid=(left+right)/2;
-        //    if(nums[mid]<target){
-        //        left=mid+1;
-        //        if(nums[left]==target){
-        //            return left;
-        //        }
-        //    }else if(nums[mid]>target){
-        //        right=mid-1;
-        //        if(nums[right]==target){
-        //            return right;
-        //        }
-        //    }
-        // }
-        int first=-1;
-        int last=-1;
-       for(int i=0;i<nums.length;i++){
-           if(nums[i]==target){
-               if(first==-1){
-                   first=i;
-                   last=i;
-               }else{
-                   last=i;
-               }
-           }
-       }
-       int[]position={first,last};
-       return position;
+     return new int[]{lb,upperbound(nums,target)-1};
     }
+
+        public static int lowerbound(int[]nums,int target ){
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
+        int ans=n;
+        while(left<=right){
+           int mid=(left+right)/2;
+           if(nums[mid]>=target){
+               ans=mid;
+               right=mid-1;
+              } else{
+               left=mid+1;
+                }
+          
+           }
+           return ans;
+    }
+            public static int upperbound(int[]nums,int target ){
+                int n=nums.length;
+        int left=0;
+        int right=n-1;
+        int ans=n;
+            while(left<=right){
+           int mid=(left+right)/2;
+           if(nums[mid]>target){
+               ans=mid;
+               right=mid-1;
+              } else{
+               left=mid+1;
+                }
+          
+           }
+           return ans;
+        }
 }
+        //brute force approach
+//         int first=-1;
+//         int last=-1;
+//        for(int i=0;i<nums.length;i++){
+//            if(nums[i]==target){
+//                if(first==-1){
+//                    first=i;
+//                    last=i;
+//                }else{
+//                    last=i;
+//                }
+//            }
+//        }
+//        int[]position={first,last};
+//        return position;
+//     }
+// }
