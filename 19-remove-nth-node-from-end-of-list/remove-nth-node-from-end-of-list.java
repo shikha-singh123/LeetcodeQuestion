4fcde8664de627a6a2446b46e071c42689bb23ff
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2024-01-17 15:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (5.69%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,29 +10,44 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int count=0;
-        ListNode temp=head;
-        while(temp!=null){
-            count++;
-            temp=temp.next;
+        // *********Brute force approach
+    //     int count=0;
+    //     ListNode temp=head;
+    //     while(temp!=null){
+    //         count++;
+    //         temp=temp.next;
 
-        }   
-        if(count==n){
-       ListNode newHead=head.next;
-        return newHead;
-        }
-        int res=count-n;
-          temp=head;
-        while(temp!=null){
-            res--;
-            if(res==0)
-            break;
-            temp=temp.next;
+    //     }   
+    //     if(count==n){
+    //    ListNode newHead=head.next;
+    //     return newHead;
+    //     }
+    //     int res=count-n;
+    //       temp=head;
+    //     while(temp!=null){
+    //         res--;
+    //         if(res==0)
+    //         break;
+    //         temp=temp.next;
 
+    //     }
+    //     ListNode delNode=temp.next;
+    //     temp.next=temp.next.next;
+    //     return head;
+
+    ListNode fast=head;
+            ListNode slow=head;
+    for(int i=0;i<n;i++)
+        fast=fast.next;
+            if(fast==null) return head.next;
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
         }
-        ListNode delNode=temp.next;
-        temp.next=temp.next.next;
+            ListNode delNode=slow.next;
+            slow.next=slow.next.next;
         return head;
+        }
+
 
     }
-}
