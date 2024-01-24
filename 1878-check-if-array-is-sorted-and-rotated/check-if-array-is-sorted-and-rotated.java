f14f86451104fff4d4f53b82675191d8e2ab5bf4
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2024-01-24 18:13:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (10.16%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,33 +1,32 @@
 class Solution {
     public boolean check(int[] nums) {
-        int n=nums.length;
-        boolean isIncreasing=true;
-        int pivot=0;
+       int n=nums.length;
+       int pivot=0;
+       boolean isIncreasing =true;
+       for(int i=1;i<n;i++){
+           if(nums[i]<nums[i-1]){
+              pivot=i;
+               isIncreasing =false;
+               break;
+           }
+       }
+       if(isIncreasing){
+           return true;
+       }
+       int rotation=n-pivot;
+       if(rotation>n){
+           return false;
+       }
+       int rotatedArray[]=new int[n];
+        for(int i=0;i<n;i++){
+            rotatedArray[(i+rotation)%n]=nums[i];
+        }
         for(int i=1;i<n;i++){
-            if(nums[i]<nums[i-1]){
-                pivot=i;
-                isIncreasing=false;
-                break;
-            }
-
-        }
-        if(isIncreasing){
-            return true;
-        }
-        int rotation=n-pivot;
-        if (rotation>n){
+            if(rotatedArray[i]<rotatedArray[i-1])
             return false;
         }
-        int[]rotatedArray=new int[n];
-         for(int i=0;i<n;i++){
-             rotatedArray[(i+rotation)%n]=nums[i];
-             
-         }
-         for(int i=1;i<n;i++){
-             if(rotatedArray[i]<rotatedArray[i-1]){
-             return false;
-         }
-    }
-    return true;
+        return true;
+      
+
 }
 }
