16837afc0fffcdd6d8c1336179b6194924421a61
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2024-07-13 08:42:08 +0530

    Time: 6 ms (24.94%) | Memory: 41.4 MB (6.44%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        String ans="";
+        while(columnNumber>0){
+            columnNumber--;
+            ans=(char)(columnNumber%26+'A')+ans;
+            columnNumber=columnNumber/26;
+        }
+        return ans;
+    }
+}
