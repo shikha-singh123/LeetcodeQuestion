ac30b03a98a39e55acf9af0ecac8beac8e0d44d5
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2024-09-19 14:00:53 +0530

    Time: 2 ms (58.94%) | Memory: 41.9 MB (33.56%) - LeetSync

---
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countElements(int[] nums) {
+       int count=0;
+       Arrays.sort(nums);
+       int l=nums[0];
+       int r=nums[nums.length-1];
+       for(int i=1;i<nums.length-1;i++){
+        if(nums[i]>l && nums[i]<r){
+            count++;
+        }
+       }
+       return count;
+        
+    }
+}
