c0e163686cc7a8a0b2bd6b9b5c85601bf7eda0f8
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2024-09-26 11:31:52 +0530

    Time: 12 ms (12.16%) | Memory: 44.7 MB (24.40%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+       Arrays.stream(nums).forEach(n->map.put(n,map.getOrDefault(n,0)+1));
+
+       return Arrays.stream(nums).boxed()
+               .sorted((a,b)->map.get(a)!=map.get(b)?map.get(a)-map.get(b):b-a)
+               .mapToInt(n->n)
+               .toArray();
+    }
+}
