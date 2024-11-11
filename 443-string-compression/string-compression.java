5ae9f18bc0dd14da8e421e1d926355dbe80606f2
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2024-11-11 17:18:21 +0530

    Time: 1 ms (99.66%) | Memory: 43.6 MB (87.54%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,22 +1,19 @@
 class Solution {
     public int compress(char[] chars) {
-        int ans = 0;  // Pointer to the position to write in the char array
-        int i = 0;    // Start from the first character in the array
+        int ans = 0;  
+        int i = 0;  
         
         while (i < chars.length) {
             char letter = chars[i];
             int count = 0;
             
-            // Count the frequency of the current character
             while (i < chars.length && chars[i] == letter) {
                 count++;
-                i++;  // Move to the next character
+                i++; 
             }
             
-            // Write the current character to the result array
             chars[ans++] = letter;
             
-            // If the character appears more than once, write the count
             if (count > 1) {
                 for (char c : String.valueOf(count).toCharArray()) {
                     chars[ans++] = c;
@@ -24,6 +21,6 @@ class Solution {
             }
         }
         
-        return ans;  // Return the new length of the array after compression
+        return ans;  
     }
 }
