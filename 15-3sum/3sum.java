1e03d83789a008a3310190e8fb4ab59c65fbdbbb
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2024-11-14 12:22:55 +0530

    Time: 28 ms (78.32%) | Memory: 51.3 MB (64.27%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,39 +1,26 @@
 class Solution {
-        List<List<Integer>>result=new ArrayList<>();
     public List<List<Integer>> threeSum(int[] nums) {
-        int n=nums.length;
-        if(nums.length<3 || nums==null)
-        return result;
+        List<List<Integer>> list=new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            if(i==0 || nums[i-1]!=nums[i]){
-                twoSortedArray(i+1,n-1,nums,0-nums[i]);
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int total=nums[i]+nums[j]+nums[k];
+                if(total<0){
+                    j++;
+                }else if(total>0){
+                    k--;
+                }else{
+                    list.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    while(nums[j]==nums[j-1] && j<k){
+                        j++;
+                    }
+                }
             }
         }
-        return result;
-
+        return list;
+         }
     }
-    void twoSortedArray(int i,int j,int[]nums,int target){
-        int a1=nums[i-1];
-        while(i<j){
-            if(nums[i]+nums[j]>target){
-                j--;
-            }else if(nums[i]+nums[j]<target){
-                i++;
-            }else{
-                List<Integer>list=new ArrayList();
-                list.add(a1);
-                list.add(nums[i]);
-                list.add(nums[j]);
-                result.add(list);
-                //duplicate b
-                while(i<j && nums[i]==nums[i+1])i++;
-                //duplicate c
-                while(i<j && nums[j]==nums[j-1])j--;
-                i++;
-                j--;
-            
-        }
-    }
-}
-}
