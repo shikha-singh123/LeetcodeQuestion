ad8c70425e3a27b9da580385e21e70090249ebe2
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2024-11-17 10:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (19.12%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,61 +1,30 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-     int lb=lowerbound(nums,target);
-     if(lb==nums.length || nums[lb]!=target)
-     return new int []{-1,-1};
+       int first=findPosition(nums,target,true);
+       int last=findPosition(nums,target,false);
+       return new int[]{first,last};
 
-     return new int[]{lb,upperbound(nums,target)-1};
     }
-
-        public static int lowerbound(int[]nums,int target ){
-        int n=nums.length;
+    private int findPosition(int []nums,int target,boolean findFirst){
         int left=0;
-        int right=n-1;
-        int ans=n;
+        int right=nums.length-1;
+        int pos=-1;
         while(left<=right){
-           int mid=(left+right)/2;
-           if(nums[mid]>=target){
-               ans=mid;
-               right=mid-1;
-              } else{
-               left=mid+1;
-                }
-          
-           }
-           return ans;
-    }
-            public static int upperbound(int[]nums,int target ){
-                int n=nums.length;
-        int left=0;
-        int right=n-1;
-        int ans=n;
-            while(left<=right){
-           int mid=(left+right)/2;
-           if(nums[mid]>target){
-               ans=mid;
-               right=mid-1;
-              } else{
-               left=mid+1;
-                }
-          
-           }
-           return ans;
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target){
+             pos=mid;
+        if(findFirst)
+        right=mid-1;
+        else{
+            left=mid+1;
         }
+    }
+    else if(nums[mid]<target){
+         left=mid+1;
+    }else{
+        right=mid-1;
+    }
 }
-        //brute force approach
-//         int first=-1;
-//         int last=-1;
-//        for(int i=0;i<nums.length;i++){
-//            if(nums[i]==target){
-//                if(first==-1){
-//                    first=i;
-//                    last=i;
-//                }else{
-//                    last=i;
-//                }
-//            }
-//        }
-//        int[]position={first,last};
-//        return position;
-//     }
-// }
+return pos;
+    }
+}
