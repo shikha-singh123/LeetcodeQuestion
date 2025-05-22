b128a3d47bdd29c26912ed9c3eb175c77d4f6d33
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2025-05-22 09:32:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (29.24%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -8,40 +8,24 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.util.Stack;
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // Brute force approach
-    //     ListNode temp=head;
-    //     Stack<Integer>st= new Stack<>();
-    //      while(temp!=null){
-    //     st.push(temp.val);
-    //     temp=temp.next;
-    //     }
+        if(head==null){
+            return null;
+        }
+        if(head.next==head){
+            return head;
+        }
+        ListNode preNode=null;
+        ListNode currNode=head;
+        while(currNode!=null){
+            ListNode nextNode=currNode.next;
+            currNode.next=preNode;
+            preNode=currNode;
+            currNode=nextNode;
 
-    //     temp=head;
-    //      while(temp!=null){
-    //     temp.val= st.pop();
-    //     temp=temp.next;
-    // }
-    // return head;
-    // <-------Time Complexity O(n)-------->
-        //   ListNode temp=head;
-        //   ListNode pre=null;
-        //   while(temp!=null){
-        //      ListNode front=temp.next;
-        //       temp.next=pre;
-        //       pre=temp;
-        //       temp=front;
-        //   }
-        //   return pre;
-// <---Time Complexity O(N)
-      if(head==null || head.next==null)return head;
-      ListNode newHead = reverseList(head.next);
-      ListNode front=head.next;
-      front.next=head;
-      head.next=null;
-      return newHead;
+            head=preNode;
+        }
+        return head;
     }
-
 }
