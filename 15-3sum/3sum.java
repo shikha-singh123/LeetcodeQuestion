c3f1a4ac66857a1aa4ac09981b7503ea80d8812a
shikha-singh123 <134131831+shikha-singh123@users.noreply.github.com>
2025-11-24 16:18:36 +0530

    Time: 33 ms (43.05%) | Memory: 59.8 MB (8.03%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,40 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>>res=new ArrayList<>();
-         
-         if(nums==null || nums.length<3) return res;
+        List<List<Integer>> boxedRes = new ArrayList<>();
+        if (nums == null || nums.length < 3) return boxedRes;
 
-         Arrays.sort(nums);
-       int n=nums.length;
+        Arrays.sort(nums);
+        int n = nums.length;
 
-for(int i=0;i<n-2;i++){
-    if(i>0 && nums[i]==nums[i-1])continue;
+        // Store primitives first to avoid boxing during the scan
+        List<int[]> primitiveTriplets = new ArrayList<>();
 
-    int target=-nums[i];
-    int left=i+1;
-    int right=n-1;
-    while(left<right){
-        int sum=nums[left]+nums[right];
-        if(sum==target){
-            res.add(Arrays.asList(nums[i],nums[left],nums[right]));
-            left++;
-            right--;
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] > 0) break; // early exit: remaining numbers can't sum to 0
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            while(left<right && nums[left]==nums[left-1]){
-                left++;
+            int target = -nums[i];
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    primitiveTriplets.add(new int[] { nums[i], nums[left], nums[right] });
+                    left++;
+                    right--;
+                    // skip duplicates
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
-            while(left<right && nums[right]==nums[right+1]){
-                right--;
-            }
-        }else if(sum<target){
-            left++;
-        }else{
-            right--;
         }
-    }
 
-}
-return res;
-
+        // Convert primitive triplets to List<List<Integer>> only once, at the end
+        boxedRes = new ArrayList<>(primitiveTriplets.size());
+        for (int[] t : primitiveTriplets) {
+            List<Integer> trip = new ArrayList<>(3);
+            trip.add(t[0]);
+            trip.add(t[1]);
+            trip.add(t[2]);
+            boxedRes.add(trip);
+        }
+        return boxedRes;
     }
 }
